3c7c5fcb131b51223851a47840700d979380ae01
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-05-03 14:54:17 +0200

    hobbies ex.

--- app.js
+++ app.js
@@ -4,4 +4,33 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.static("public"));
+
 // Build the server here
+app.get("/", (request, response) =>
+  response.sendFile(__dirname + "/views/index.html")
+);
+
+app.get("/homepage", (req, res) => 
+    res.sendFile(__dirname + "/views/home.html")
+);
+
+app.get("/about", (req, res) => 
+    res.sendFile(__dirname + "/views/about.html")
+);
+
+// hobbies
+
+app.get("/hobbies", (req, res) => {
+    res.sendFile(__dirname + "/views/hobbies.html")
+
+})
+
+app.get("/favorite/hobbies", (req, res) => {
+    res.sendFile(__dirname + "/views/favoritehobbies.html")
+
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('running on port 3000'));
+
